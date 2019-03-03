6178671d565c78988506d477c17895183dc3d9fa
susiejss <jian.s@husky.neu.edu>
2019-03-03 15:17:41 -0800

    use treemap: index as key, in order to move slow pointer directly to last index of furthest element

--- 2pointers/slidingWindow/LongSubstringWithKCharacters.java
+++ 2pointers/slidingWindow/LongSubstringWithKCharacters.java
@@ -29,3 +29,49 @@ public class Solution {
     return freq.size() < k ? null : input.substring(maxPos[0], maxPos[1] + 1);
   }
 }
+
+
+//2 pointers with non-fixed size
+//a little improvement: use treemap to move slow directly to last index of furthest element
+//treemap<index, character>   revered to map<character, index>
+//T: same -> O(n)
+
+public class Solution {
+  public String longest(String input, int k) {
+    if (k == 0 && input.length() == 0) return "";
+    if (input == null || input.length() == 0) return null;
+    
+    int fast = 0;
+    int slow = -1;//always points to the last index before valid sliding window
+    int[] maxPos = new int[]{0,0};
+    //map records the updated lastest index of the chatacter
+    Map<Character, Integer> lastIndex = new HashMap<>();
+    TreeMap<Integer, Character> reverse = new TreeMap<>();
+    while (fast < input.length()) {
+      char ch = input.charAt(fast);
+      Integer lastOccur = lastIndex.get(ch);
+      //first, update the reverse: lastOccur!=null, the letter occurred.
+      if (lastOccur != null){
+        reverse.remove(lastOccur);
+      }
+      reverse.put(fast, ch);
+      lastIndex.put(ch, fast);
+      //check if need to move slow pointer
+      if (lastIndex.size() > k) {
+        //look for the last index of furthest letter, remove it from map and treemap
+        Integer removeIdx = reverse.firstKey();
+        Character removeChar = reverse.get(removeIdx);
+        lastIndex.remove(removeChar);
+        reverse.remove(removeIdx);
+        slow = removeIdx;
+      }
+      //check if need to update global max position: length = fast - slow
+      if (fast - slow > maxPos[1] - maxPos[0] + 1){
+        maxPos[0] = slow + 1;
+        maxPos[1] = fast;
+      }
+      fast++;
+    }
+    return lastIndex.size() < k ? null : input.substring(maxPos[0], maxPos[1] + 1);
+  }
+}
