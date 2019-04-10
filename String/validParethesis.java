64a63cfb59b0e6c9fc0ad3f919b00e412621e040
susiejss <jian.s@husky.neu.edu>
2019-04-09 20:05:04 -0700

    valid parenthesis using stack, only put right parenthesis

---
+++ String/validParethesis.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean isValidParentheses(String s) {
+		Deque<Character> stack = new LinkedList<Character>();
+		for (char c : s.toCharArray()) {
+			if (c == '(')
+				stack.push(')');
+			else if (c == '{')
+				stack.push('}');
+			else if (c == '[')
+				stack.push(']');
+			else if (stack.isEmpty() || stack.pop() != c)
+				return false;
+		}
+		return stack.isEmpty();
+	}
+}
