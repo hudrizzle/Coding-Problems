ed99df16d3f3d6c8196cc8a68dca7a12fab90d1e
susiejss <jian.s@husky.neu.edu>
2019-04-16 18:21:16 -0700

    LRU cache

---
+++ classics/CombineDS/LRUcache.java
@@ -0,0 +1,81 @@
+/*doubly linkedlist + hashtable 
+*use cases: 1. K exists, return V and adjust to head(newest);
+*2. cache is full, remove node at the tail of LinkedList
+*3. insert a new node<K,V> to the newest position
+*when adjusting to newest and remove oldest(tail), both require O1 time ->doubly linked list since single LL can't track previous node
+*wanna randomly access node in O1 -> hashtable(key, reference to node(key,value))
+*/
+public class Solution<K, V> {
+  //Node of linkedlist must be double directions for O1 adjusting
+  public static class Node<K,V> {
+    private K key;
+    private V value;
+    private Node<K,V> prev;
+    private Node<K,V> next;
+    public Node(K key, V value) {
+      this.key = key;
+      this.value = value;
+    }
+  }
+  //since limit is pre-defined, cannot be changed
+  private final int limit;
+  private Node<K,V> head;
+  private Node<K,V> tail;
+  private Map<K, Node<K,V>> map;
+  // limit is the max capacity of the cache
+  public Solution(int limit) {
+    this.limit = limit;
+    this.map = new HashMap<K, Node<K,V>>();
+  }
+  
+  //assume the newst node is placed in the head of LinkedList
+  public void set(K key, V value) {
+    Node<K,V> cur = map.get(key);
+    if (cur != null) { // key exists in map, just update its value and adjust its position
+      cur.value = value;
+      //package the remove operation
+      remove(cur);
+    }else {//key not in map, check size of cache, add this new node
+      if (map.size() == limit) {//remove the node at the tail
+        remove(tail);
+        cur = new Node<K,V>(key,value);
+      }else {//directly append this new node
+        cur = new Node<K,V>(key,value);
+      }
+    }
+    append(cur);
+  }
+  //not to sync with map when remove or append node from LL
+  //remove the node from LL: first remove edges pointing to node, then update head and tail
+  private Node<K,V> remove(Node<K,V> node){
+    map.remove(node.key);
+    if (node.prev != null) node.prev.next = node.next;
+    if (node.next != null) node.next.prev = node.prev;
+    if (head == node) head = node.next;
+    if (tail == node) tail = node.prev;
+    return node;
+  }
+  //append the node to the newest position- head of LL
+  private Node<K,V> append(Node<K,V> node){
+    map.put(node.key, node);
+    if (head == null){//LL is empty
+      head = tail = node;
+    }else {
+      node.next = head;
+      head.prev = node;
+      head = node;
+    }
+    return node;
+  }
+  
+  public V get(K key) {
+    if (map.containsKey(key)){
+      Node<K,V> cur = map.get(key);
+      remove(cur);
+      append(cur);
+      return cur.value;
+    }else {
+      return null;
+    }
+  }
+}
