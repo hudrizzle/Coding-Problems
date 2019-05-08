dd2e503a83e7bfb8c105821c29e7696661780a3f
susiejss <jian.s@husky.neu.edu>
2019-05-08 02:07:15 -0700

    improved code

--- dfs/flipgame.java
+++ dfs/flipgame.java
@@ -26,3 +26,39 @@ public class Solution {
     return allres;
   }
 }
+
+//with momerization, dedup process, improve time. improved code
+public class Solution {
+  public boolean canWin(String input) {
+    Map<String, Boolean> memo = new HashMap<>();
+    return canWin(input, memo);
+  }
+  public boolean canWin(String input, Map<String, Boolean> memo) {
+    List<String> next = generateNextMoves(input);
+    //base case
+    if (next == null) {
+      memo.put(input, false);
+      return false;
+    }
+    //as long as there's only 1 next string that can garantee the other person lose, then I'll win
+    for (String ne : next) {
+      if (memo.get(ne) != null && memo.get(ne) == false || !canWin(ne)) {
+        memo.put(ne, true);
+        return true;
+      }
+    }
+    memo.put(input, false);
+    return false;
+  }
+  private List<String> generateNextMoves(String input) {
+    List<String> allres = new ArrayList<>();
+    if (input == null || input.length() < 2) return allres;
+    for (int i = 0; i < input.length() - 1; i++) {
+      if (input.charAt(i) == '+' && input.charAt(i + 1) == '+') {
+        String temp = input.substring(0, i) + "--" + input.substring(i + 2);
+        allres.add(temp);
+      }
+    }
+    return allres;
+  }
+}
