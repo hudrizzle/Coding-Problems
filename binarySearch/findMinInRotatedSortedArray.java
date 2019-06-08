c5969634af7c8afb0a472aa7b9c07a07ac398dbc
susiejss <jian.s@husky.neu.edu>
2019-06-07 18:03:41 -0700

    find min in rotated sorted array using iteratively binary search

--- binarySearch/findMinInRotatedSortedArray.java
+++ binarySearch/findMinInRotatedSortedArray.java
@@ -28,3 +28,23 @@ public class Solution {
         }
     }
 }
+
+    /**
+     * @param nums: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    //refer to classic BS, find min value in array, each time rule out half range
+    public int findMin(int[] nums) {
+        //corner cases: empty array or non-rotated array
+        if (nums == null || nums.length == 0) return -1;
+        //deal with rotated array
+        int l = 0, r = nums.length - 1;
+        while (l  + 1 < r) {
+            if (nums[l] < nums[r]) return nums[l];
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < nums[r]) r = mid;
+            else if (nums[mid] > nums[l]) l = mid + 1;
+        }
+        //only 2 elements left to be checked
+        return nums[l] < nums[r] ? nums[l] : nums[r];
+    }
