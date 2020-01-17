6276cda42cbab3c850511703be53f4576d5b3bcd
shaoshuang <shaoshuang_jian@apple.com>
2020-01-16 23:50:43 -0800

    min window containing all characters

--- slidingWindow/minWindowSubstring.java
+++ slidingWindow/minWindowSubstring.java
@@ -56,3 +56,56 @@ public class Solution {
     return freq;
   }
 }
+
+/**
+ * 1.16 solution: NOTE THE MOVING THE `fast` and  `slow`
+ */
+
+public class Solution {
+  public String minWindow(String source, String target) {
+    if (source == null || target == null || source.length() == 0 || target.length() == 0) return "";
+    Map<Character, Integer> freq = getFreq(target);
+    int fast = 0;
+    int slow = 0;
+    int matched = 0;
+    int minLen = Integer.MAX_VALUE, start = 0;
+    while (fast < source.length()) {
+      Integer cnt = freq.get(source.charAt(fast));
+      if (cnt != null) {//found the target character
+        freq.put(source.charAt(fast), cnt - 1);
+        if (cnt == 1) matched++;
+      }
+      //check left: need to move slow++, until not satisfying this condition
+      while (matched == freq.size()) {
+        //update the result if needed
+        if (minLen > fast - slow + 1) {
+          minLen = fast - slow + 1;
+          start = slow;
+        }
+        Integer con = freq.get(source.charAt(slow));
+        if (con != null) {//slow is pointing to a target character
+          freq.put(source.charAt(slow), con + 1);
+          if (con == 0) matched--;
+        }
+        slow++;//need to advance slow anyway
+      }
+      fast++;
+    }
+    return minLen == Integer.MAX_VALUE ? "" : source.substring(start, start + minLen);
+  }
+
+  private Map<Character, Integer> getFreq(String target) {
+    Map<Character, Integer> freq = new HashMap<>();
+    for (char ch : target.toCharArray()) {
+      Integer cnt = freq.get(ch);
+      if (cnt == null) {
+        freq.put(ch, 1);
+      }else {
+        freq.put(ch, cnt + 1);
+      }
+    }
+    return freq;
+  }
+}
+
+
